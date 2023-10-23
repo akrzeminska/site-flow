300f5daa40fceb61b66b41a7b3def96939c6f2b1
Adelina Krzemińska <adelina.krzeminska@gmail.com>
2023-10-23 20:30:03 +0200

    added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,21 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PageNotFoundComponent } from './core/components/page-not-found/page-not-found.component';
-import { ConstructionsComponent } from './layout/constructions/constructions.component';
-import { ContactsComponent } from './layout/contacts/contacts.component';
 import { CostEstimatesComponent } from './features/cost-estimates/cost-estimates.component';
 import { TasksComponent } from './features/tasks/tasks.component';
 import { UserPanelComponent } from './features/user-panel/user-panel.component';
+import { ConstructionsComponent } from './features/constructions/constructions.component';
+import { ContactsComponent } from './features/contacts/contacts.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PageNotFoundComponent,
-    ConstructionsComponent,
-    ContactsComponent,
     CostEstimatesComponent,
     TasksComponent,
-    UserPanelComponent
+    UserPanelComponent,
+    ConstructionsComponent,
+    ContactsComponent
   ],
   imports: [
     BrowserModule,

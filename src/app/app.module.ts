4d8b5a17f07d94dba7ec6a53b6153c0e1cffc9ac
Adelina Krzemińska <adelina.krzeminska@gmail.com>
2023-10-25 13:23:58 +0200

    cleans up the styles in the layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,6 +20,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatDividerModule } from '@angular/material/divider';
 
+
 @NgModule({
   declarations: [
     AppComponent,

aadd564f0f6c96c4ad13b8de335facf8e4eabe87
Adelina Krzemińska <adelina.krzeminska@gmail.com>
2023-11-02 19:26:52 +0100

    adds DataSource and Pagination

--- src/app/features/constructions/constructions.component.ts
+++ src/app/features/constructions/constructions.component.ts
@@ -1,59 +1,71 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { LocalStorageSeederService } from 'src/app/shared/services/local-storage-seeder.service';
 import { ConstructionsService } from './services/constructions.service';
 import { Construction } from 'src/app/models/construction.model';
-import { ConstructionsLocalStorageService } from './services/constructions-local-storage.service';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-constructions',
   templateUrl: './constructions.component.html',
   styleUrls: ['./constructions.component.scss']
 })
-export class ConstructionsComponent  {
+export class ConstructionsComponent implements OnInit, AfterViewInit {
+
+  dataSource: MatTableDataSource<Construction>;
+  
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
 
   constructor(private seederService: LocalStorageSeederService, private constructionsService: ConstructionsService){
     seederService.ensureDataSeeder();
+    this.dataSource = new MatTableDataSource<Construction>();
+  }
+  ngAfterViewInit(): void {
+    this.dataSource.paginator = this.paginator;
+  }
+  ngOnInit(): void {
+    this.getAllData();
   }
 
   columns = [
     {
       columnDef: 'position',
-      header: 'No.',
+      header: 'Nr',
       cell: (element: Construction) => `${element.id}`,
     },
     {
       columnDef: 'name',
-      header: 'Name',
+      header: 'Nazwa',
       cell: (element: Construction) => `${element.name}`,
     },
     {
       columnDef: 'location',
-      header: 'Location',
+      header: 'Lokalizacja',
       cell: (element: Construction) => `${element.location}`,
     },
     {
       columnDef: 'client',
-      header: 'Client',
+      header: 'Klient',
       cell: (element: Construction) => `${element.client}`,
     },
     {
       columnDef: 'cost',
-      header: 'Cost',
+      header: 'Kosztorys',
       cell: (element: Construction) => `${element.costEstimateId}`,
     },
     {
       columnDef: 'manager',
-      header: 'Manager',
+      header: 'Kierownik projektu',
       cell: (element: Construction) => `${element.manager}`,
     },
     {
       columnDef: 'contact',
-      header: 'Contact',
+      header: 'Kontakty',
       cell: (element: Construction) => `${element.contactId}`,
     },
     {
       columnDef: 'desription',
-      header: 'Description',
+      header: 'Opis',
       cell: (element: Construction) => `${element.description}`,
     },
     {
@@ -63,28 +75,28 @@ export class ConstructionsComponent  {
     },
   ];
 
-  dataSource: Construction[] = [];
+  
   displayedColumns = this.columns.map(c => c.columnDef);
 
   getDataById() {
     const constructionId: number = 1;
-    
+
     this.constructionsService.getById(constructionId).subscribe((construction: any) => {
       if (construction) {
-        this.dataSource = [construction];
+        this.dataSource.data = [construction];
       } else {
-        console.log('Nie znaleziono konstrukcji o podanym identyfikatorze.');
+        console.log('Nie znaleziono konstrukcji o podanym id.');
       }
     });
   }
 
   getAllData() {
-    this.constructionsService.getAll().subscribe((constructions:Array<any>)=> {
+    this.constructionsService.getAll().subscribe((constructions: Array<Construction>) => {
       if (constructions) {
-        this.dataSource = constructions;
+        this.dataSource.data = constructions;
       } else {
-        console.log('Nie znaleziono konstrukcji o podanym identyfikatorze.');
+        console.log('Nie znaleziono konstrukcji o podanym id.');
       }
-    })
+    });
   }
 }

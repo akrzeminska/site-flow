06641ca858e5dbf88162515ad8eb11f5157d3164
Adelina Krzemińska <adelina.krzeminska@gmail.com>
2023-11-09 13:46:54 +0100

    status changes and activation of the save button

--- src/app/features/constructions/components/feature-dialog/feature-dialog.component.ts
+++ src/app/features/constructions/components/feature-dialog/feature-dialog.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, NonNullableFormBuilder, Validators } from '@angular/forms';
 import { MatDialogRef } from '@angular/material/dialog';
 import { Construction } from 'src/app/models/construction.model';
 
@@ -10,12 +10,13 @@ import { Construction } from 'src/app/models/construction.model';
 })
 export class FeatureDialogComponent implements OnInit {
   constructionForm!: FormGroup;
+  saveButtonDisabled!: boolean;
   costSelect: string[] = ['brak', 'Kosztorys nr 1', 'Kosztorys nr 2', 'Kosztorys nr 3'];
   contactList: string[] = ['contact_1', 'contact_2', 'contact_3', 'contact_4'];
   taskList: string[] = ['task_1', 'task_2', 'task_3', 'task_4'];
   statusOptions: string[] = ['In progress', 'Planning', 'Completed'];
 
-  constructor(private dialogRef: MatDialogRef<FeatureDialogComponent>, private formBuilder: FormBuilder) {}
+  constructor(private dialogRef: MatDialogRef<FeatureDialogComponent>, private formBuilder: NonNullableFormBuilder) {}
 
   ngOnInit(): void {
     this.constructionForm = this.formBuilder.group({
@@ -30,13 +31,17 @@ export class FeatureDialogComponent implements OnInit {
       taskId: ['', Validators.required],
       status: ['', Validators.required]
     });
-  }
 
-  save() {
-    if (this.constructionForm.valid) {
-      const constructionData: Construction = this.constructionForm.value;
-      this.dialogRef.close(constructionData);
-    }
+    this.saveButtonDisabled = true;
+
+    this.constructionForm.statusChanges.subscribe((status) => {
+      if (status === 'VALID') {
+        this.saveButtonDisabled = false;
+      } else {
+        this.saveButtonDisabled = true;
+      }
+    })
+    
   }
 
   dismiss() {
@@ -45,7 +50,9 @@ export class FeatureDialogComponent implements OnInit {
 
   onSubmit() {
     if (this.constructionForm.valid) {
-      console.log(this.constructionForm.value);
+      const newConstructionData: Construction = this.constructionForm.value;
+      this.dialogRef.close(newConstructionData);
     }
   }
+
 }

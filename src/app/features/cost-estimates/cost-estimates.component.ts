664242cfb408cb7d5888ef2b2e582b0216e36c5f
Adelina Krzemińska <adelina.krzeminska@gmail.com>
2023-12-11 19:16:31 +0100

    creates atomatic expansion panel with *ngFor

--- src/app/features/cost-estimates/cost-estimates.component.ts
+++ src/app/features/cost-estimates/cost-estimates.component.ts
@@ -1,8 +1,10 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatAccordion } from '@angular/material/expansion';
 import {MatTableModule} from '@angular/material/table';
 import { Router } from '@angular/router';
 import { CostEstimatesService } from './services/cost-estimates.service';
+import { CostEstimate } from 'src/app/models/cost-estimate.model';
+import { LocalStorageSeederService } from 'src/app/shared/services/local-storage-seeder.service';
 
 @Component({
   selector: 'app-cost-estimates',
@@ -10,13 +12,34 @@ import { CostEstimatesService } from './services/cost-estimates.service';
   styleUrls: ['./cost-estimates.component.scss']
 })
 
-export class CostEstimatesComponent {
-
-
-  constructor(private router: Router) {}
-
-  @ViewChild(MatAccordion) accordion!: MatAccordion;
+export class CostEstimatesComponent implements OnInit {
+  dataCostEstimates: Array<CostEstimate> = [];
   
+  @ViewChild(MatAccordion) accordion!: MatAccordion;
+
+  constructor(private router: Router,
+    private seederService: LocalStorageSeederService,
+    private costEstimatesService: CostEstimatesService) {
+      seederService.ensureDataSeeder();
+    }
+
+  ngOnInit(): void {
+    this.getAllData();
+  }
+
+  getAllData() {
+    this.costEstimatesService
+      .getAll()
+      .subscribe((costEstimate: Array<CostEstimate>) => {
+        if (costEstimate) {
+          this.dataCostEstimates = costEstimate;
+        } else {
+          console.log('Nie znaleziono kosztorysów.');
+        }
+        console.log('Dane po pobraniu przez getAllData:', this.dataCostEstimates);
+      });
+  }
+
   applyFilter(event: Event) {
   }
 
@@ -35,5 +58,4 @@ export class CostEstimatesComponent {
   navigateToDetails(id: number) {
     this.router.navigate(['/costs-estimates/', id.toString()]);
   }
-
 }

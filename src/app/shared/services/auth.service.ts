fd1ea9e8a09ada167ec3cd305d80df380dfb640a
Adelina Krzemińska <adelina.krzeminska@gmail.com>
2024-01-02 21:02:17 +0100

    Introduces application-ready login via Google's OAuth2

--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -1,32 +1,54 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { AuthConfig, OAuthService } from 'angular-oauth2-oidc';
+import { Observable, Subject } from 'rxjs';
+
+const oAuthConfig: AuthConfig = {
+  issuer: 'https://accounts.google.com',
+  strictDiscoveryDocumentValidation: false,
+  redirectUri: 'http://localhost:4200',
+  clientId: '876890439510-kc14p3sr0rq0nvhafru7umaeld578qpj.apps.googleusercontent.com',
+  scope: 'openid profile email https://www.googleapis.com/auth/gmail.readonly'
+}
+
+export interface UserInfo {
+  info: {
+    sub: string,
+    email: string,
+    name: string,
+    picture: string
+  }
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor(private oauthService: OAuthService) {
-    this.configureOAuth();
+  userProfileSubject = new Subject<UserInfo>();
+
+  constructor(private readonly oAuthService: OAuthService) {
+    oAuthService.configure(oAuthConfig);
+    oAuthService.logoutUrl = 'https://www.google.com/accounts/Logout';
+    oAuthService.loadDiscoveryDocument().then(() => {
+          // Rozpocznij przepływ niejawnego logowania
+      oAuthService.tryLoginImplicitFlow().then(() => {
+          // Sprawdź stan tokena i profil użytkownika po powrocie z przepływu
+        if(!oAuthService.hasValidAccessToken()) {
+          oAuthService.initLoginFlow();
+        } else {
+          oAuthService.loadUserProfile().then((userProfile) => {console.log(JSON.stringify(userProfile));
+          })
+        }
+      })
+    })
   }
 
-  private configureOAuth() {
-    const authConfig: AuthConfig = {
-      issuer: 'https://accounts.google.com',
-      redirectUri: 'http://localhost:4200/login',
-      clientId: '876890439510-kc14p3sr0rq0nvhafru7umaeld578qpj.apps.googleusercontent.com',
-      scope: 'openid profile email'
-    };
-
-    this.oauthService.configure(authConfig);
-    this.oauthService.loadDiscoveryDocumentAndTryLogin();
+  isLoggedIn(): boolean {
+    return this.oAuthService.hasValidAccessToken();
   }
 
-  login() {
-    this.oauthService.initImplicitFlow();
-  }
-
-  logOut() {
-    this.oauthService.logOut();
+  signOut() {
+    this.oAuthService.logOut();
   }
 }

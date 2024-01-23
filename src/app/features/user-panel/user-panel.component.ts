f11cb6f0610d489f6d61c42d0878815bcd7ea12d
Adelina Krzemińska <adelina.krzeminska@gmail.com>
2024-01-23 13:49:09 +0100

    gives buttons on user-panel a function with routing

--- src/app/features/user-panel/user-panel.component.ts
+++ src/app/features/user-panel/user-panel.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-user-panel',
@@ -6,5 +7,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./user-panel.component.scss']
 })
 export class UserPanelComponent {
+  constructor(private router: Router) {}
+
+  navigateToConstructions() {
+    this.router.navigate(['constructions']);
+  }
+
+  navigateToCostEstimates() {
+    this.router.navigate(['costs-estimates']);
+  }
+
+  navigateToContacts() {
+    this.router.navigate(['contacts']);
+  }
+
+  navigateToTasks() {
+    this.router.navigate(['tasks']);
+  }
 
 }

44e526e4ac7cb173babb08b0bf13e4a5596eda2a
Adelina Krzemińska <adelina.krzeminska@gmail.com>
2024-02-27 20:54:58 +0100

    tests the existence of the component and the operation of the addNewConstruction method on the add button

--- src/app/features/constructions/constructions.component.spec.ts
+++ src/app/features/constructions/constructions.component.spec.ts
@@ -1,21 +1,48 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { MatDialog } from '@angular/material/dialog';
 import { ConstructionsComponent } from './constructions.component';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { ConstructionsModule } from './constructions.module';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+
+
 
 describe('ConstructionsComponent', () => {
   let component: ConstructionsComponent;
   let fixture: ComponentFixture<ConstructionsComponent>;
+  let dialog: MatDialog;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [ConstructionsComponent]
+      declarations: [ConstructionsComponent],
+      providers: [
+        {provide: MatDialog, useValue: {}},
+        {provide: MatSnackBar, useValue: {}},
+      ],
+      imports: [
+        MatFormFieldModule,
+        MatPaginatorModule,
+        ConstructionsModule,
+        NoopAnimationsModule
+      ]
     });
     fixture = TestBed.createComponent(ConstructionsComponent);
     component = fixture.componentInstance;
+    dialog = TestBed.inject(MatDialog);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should call addNewConstruction method on button click', () => {
+    spyOn(component, 'addNewConstruction'); //zmokowałam metodę add...
+    const addButton = fixture.debugElement.nativeElement.querySelector('button'); //znajduję przycisk
+    addButton.click(); //klikam [rzycisk]
+    fixture.detectChanges(); // aktualizuję widok po kliknięciu
+    expect(component.addNewConstruction).toHaveBeenCalled(); //sprawzam czy metoda została wykonana
+  })
 });

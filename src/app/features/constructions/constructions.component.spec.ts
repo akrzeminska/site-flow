b03fa535360e71fb3f7886ffa93d137cab5c17f1
Adelina Krzemińska <adelina.krzeminska@gmail.com>
2024-02-28 14:13:32 +0100

    create unit tests for constructions component

--- src/app/features/constructions/constructions.component.spec.ts
+++ src/app/features/constructions/constructions.component.spec.ts
@@ -1,12 +1,15 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, inject } from '@angular/core/testing';
 import { MatDialog } from '@angular/material/dialog';
 import { ConstructionsComponent } from './constructions.component';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
 import { ConstructionsModule } from './constructions.module';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
-
+import { ConstructionsService } from './services/constructions.service';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatSort } from '@angular/material/sort';
+import { MatMenu, MatMenuTrigger } from '@angular/material/menu';
 
 
 describe('ConstructionsComponent', () => {
@@ -22,10 +25,10 @@ describe('ConstructionsComponent', () => {
         {provide: MatSnackBar, useValue: {}},
       ],
       imports: [
-        MatFormFieldModule,
-        MatPaginatorModule,
-        ConstructionsModule,
-        NoopAnimationsModule
+          MatFormFieldModule,
+          MatPaginatorModule,
+          ConstructionsModule,
+          NoopAnimationsModule
       ]
     });
     fixture = TestBed.createComponent(ConstructionsComponent);
@@ -38,11 +41,36 @@ describe('ConstructionsComponent', () => {
     expect(component).toBeTruthy();
   });
 
+  it('constructions service should be injected', inject([ConstructionsService], (service: ConstructionsService) => {
+    expect(service).toBeTruthy();
+  }))
+
+  it('should initialize dataSource correctly', () => {
+    expect(component.dataSource).toBeInstanceOf(MatTableDataSource);
+  })
+
+  it('should set paginator, sort and menu correctly after view initialization', () => {
+    const mockPaginator: MatPaginator = {} as MatPaginator;
+    const mockSort: MatSort = {} as MatSort;
+    const mockMenu: MatMenuTrigger = {} as MatMenuTrigger;
+
+    component.paginator = mockPaginator;
+    component.sort = mockSort;
+    component.menu = mockMenu;
+
+    component.ngAfterViewInit();
+
+    expect(component.dataSource.paginator).toBe(mockPaginator);
+    expect(component.dataSource.sort).toBe(mockSort);
+
+  })
+
   it('should call addNewConstruction method on button click', () => {
     spyOn(component, 'addNewConstruction'); //zmokowałam metodę add...
     const addButton = fixture.debugElement.nativeElement.querySelector('button'); //znajduję przycisk
-    addButton.click(); //klikam [rzycisk]
+    addButton.click(); //klikam przycisk
     fixture.detectChanges(); // aktualizuję widok po kliknięciu
     expect(component.addNewConstruction).toHaveBeenCalled(); //sprawzam czy metoda została wykonana
-  })
+  });
+
 });
